cea58ade5166a79fd0e14ddfb07b18618c07f5f8
bhlee960712 <bhlee@watechnology.co.kr>
2023-05-10 14:46:31 +0900

    add layout

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const router = require('./routes/routes')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
-
 const port = 3000
 
-app.set("view engine", "ejs")
+//use
+app.use(router)
+app.use(expressLayouts)
 
+//set
+app.set('view engine', 'ejs')
+app.set('layout', 'layout')
+app.set('layout extractScripts', true)
+
+//get
 app.get('/', (req, res) => {
-    res.render('home')
-  })
+  res.render('home')
+})
 
-app.use(router);
-
+//listen
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
 })
